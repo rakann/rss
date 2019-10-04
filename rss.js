bf9615e5163564a60f480a67dd60a98a96eda589
rakann <55981618+rakann@users.noreply.github.com>
2019-10-04 13:44:38 +0300

    Create rss.js

---
+++ rss.js
@@ -0,0 +1,46 @@
+$(`<img src="https://chat-alwrwd.com/sico/1569851136895.gif" class="fl" style="width:100%;margin-top: 1px;background-color: white;border-radius: 10px;">
+
+<a href=" " style="width:90px;margin:2px;" class="fl mini fa fa-phone btn btn-warning"> اتصل بنا</a>
+<a href=" " style="width:85px;margin:2px;" class="fl mini fa fa-heartbeat
+btn btn-danger">التطبيق</a>
+<a href=" " style="width:87px;margin:2px;" class="fl mini fa fa-star
+btn btn-success">الإشتراكات</a>
+<a href="" style="width:90px;margin:2px;" class="fl mini fa fa-balance
+btn btn-info">قوانين الموقع</a>
+   `).insertBefore('.nav-tabs'); 
+$(".nav-tabs").addClass("fl").css("width","100%");var _0xb650=["maxlength","100","attr","textarea#tbox.fl.corner.tbox.filw"];
+
+$(`<style>
+.btn-danger {background-image: -webkit-linear-gradient(top,#c7c7c7 0,#000000 100%)!important;background-image: -o-linear-gradient(top,#d9534f 0,#c12e2a 100%)!important;background-image: -webkit-gradient(linear,left top,left bottom,from(#000000),to(#c7c7c7))!important;background-image: linear-gradient(to bottom,#c7c7c7 0,#000000 100%)!important;filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#ffd9534f', endColorstr='#ffc12e2a',GradientType=0)!important;filter: progid:DXImageTransform.Microsoft.gradient(enabled=false);background-repeat: repeat-x;border-color: #c7c7c7!important;}
+}
+</style>
+`).insertBefore('.nav-tabs');
+$(".label-primary").css("border-radius","24px 24px 24px 24px")
+$(`<link href="https://fonts.googleapis.com/css?family=Changa" rel="stylesheet">
+<marquee direction="right" width="65%" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif; height: 20px;color:#FFF7AB;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5 🌸مرحبا بكم في شـات الورود للجوال نتمنى لكم قضاء أجمل الاوقات
+</marquee></font></b></td>
+`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');
+
+$("div#tlogins button.btn.btn-primary").click(function(){var myVar = setInterval(function(){ var usmsgw = $(".pmsgc").length;if(usmsgw > 0){
+
+$(`<div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:#F7E7E7;">
+<img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url;" class="fl fitimg hand u-pic ">
+<div class="uzr fl" style="padding:0px;width:80%">
+<div style="padding:0px;width:100%;" class="fl">
+<img class="fl u-ico" alt="" src="">
+<span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand">‎  .. ' صاحب الموقع ، ،</span>
+</div>
+<br>
+<div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg break fl">
+<div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg break fl">  مرحباً بك عزيزي الزآئر 👋 <h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ `</h1> نقدّر تواجدك معنا، ونتمنى لك يوماً سعيداً إن شاء الله </div>
+</div>
+</div></div>
+
+`).appendTo('div#d2')
+clearInterval(myVar);
+}else{console.log(usmsgw)} }, 2000);})
+
+$(`<center><div><div width="99.5%" style="background-image:url();color: #000000;border-radius: 5px 5px 5px5px;border-bottom: 2px solid #ABABAB;border-top: 2px solid #ABABAB;padding-bottom: 2px!important;" ><font style="background-color: #BCC6CF;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;" >مرحبآ بكم في شآت إلورود نتمني لكم أجمل الاوقات  </font></div></div><center>`).insertBefore('#d2');$('.nav-tabs').addClass('fl').css('width','100%');$
+('a[href="https://chat-alwrwd.com/sico/1569688118667.gif"]').removeAttr('href');
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"><link rel="stylesheet" href="assl.css">
+</style>`).insertBefore('span.fl.fa.fa-send');
